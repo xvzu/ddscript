82119c9db69d2cfb1acc0242fbc8ab2a8fffca0f
xvzu <85627576+xvzu@users.noreply.github.com>
2025-07-25 00:14:41 +0800

    Update scriperepo.sh

--- scriperepo.sh
+++ scriperepo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# 将光标移到屏幕顶部
-tput clear
+# 清屏并将光标移到屏幕顶部
+clear
+stty sane
 
 # 输出脚本选项菜单
 echo "请选择要执行的操作:"
@@ -13,28 +14,32 @@ read -p "请输入选项 (01/02/00): " option
 case "$option" in
   01)
     # 运行 aaPanel 安装命令
-    tput clear  # 清屏并将光标移到顶部
+    clear
+    stty sane
     echo "正在安装 aaPanel ..."
     wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && echo "y" | bash install.sh forum && rm install.sh
     ;;
   
   02)
     # 运行安装最新版本脚本命令
-    tput clear  # 清屏并将光标移到顶部
+    clear
+    stty sane
     echo "正在安装最新版本 ..."
     curl -sSO http://io.bt.sb/install/install_latest.sh && bash install_latest.sh
     ;;
   
   00)
     # 退出脚本
-    tput clear  # 清屏并将光标移到顶部
+    clear
+    stty sane
     echo "退出脚本。"
     exit 0
     ;;
   
   *)
     # 输入无效选项
-    tput clear  # 清屏并将光标移到顶部
+    clear
+    stty sane
     echo "无效的选项，请选择 01, 02 或 00 来退出。"
     ;;
 esac
